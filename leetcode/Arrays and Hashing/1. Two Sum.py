819eade95183ea2b6f359658b1fe46fc0d3c452e
Daniel Sousa <sousa.dfs@gmail.com>
2022-04-23 04:50:30 -0300

    reorganize array and string questions by pattern

--- leetcode/Arrays and Hashing/1. Two Sum.py
+++ leetcode/Arrays and Hashing/1. Two Sum.py
@@ -5,12 +5,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''
+        """
         Cache numbers in a dictionary to get O(1) access
 
         Time complexity:  O(n)
         Space complexity: O(n)
-        '''
+        """
         seen = dict()
         for idx, n in enumerate(nums):
             want = target - n
@@ -18,6 +18,18 @@ class Solution:
                 return [idx, seen[want]]
             seen[n] = idx
         return [-1, -1]
+
+    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
+        """
+        Brute force approach
+
+        Time complexity:  O(n^2)
+        Space complexity: O(1)
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 
 # === tests ===
